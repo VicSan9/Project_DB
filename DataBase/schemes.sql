8dd40016aea2bf27ac7d9182b646d71e4af692b9
VicSan9 <svictorvf910@gmail.com>
2022-05-27 23:25:30 -0500

    Update schemes.sql

--- DataBase/schemes.sql
+++ DataBase/schemes.sql
@@ -1,21 +1,21 @@
 --Crear secuencias
 	--Secuencia id de vendedor
-CREATE SEQUENCE if not EXISTs id_vend_seq INCREMENT 1 START 1110;
+CREATE SEQUENCE IF NOT EXISTs id_vend_seq INCREMENT 1 START 1110;
 
 	--Secuencia id de miembro
-CREATE SEQUENCE if not EXISTs id_miemb_seq INCREMENT 1 START 2110;
+CREATE SEQUENCE IF NOT EXISTs id_miemb_seq INCREMENT 1 START 2110;
 
 	--Secuencia id de productos
-CREATE SEQUENCE if not EXISTs id_prod_seq INCREMENT 1 START 3110;
+CREATE SEQUENCE IF NOT EXISTs id_prod_seq INCREMENT 1 START 3110;
 
 	--Secuencia id de transacciones 
-CREATE SEQUENCE if not EXISTs id_transac_seq INCREMENT 1 START 4110;
+CREATE SEQUENCE IF NOT EXISTs id_transac_seq INCREMENT 1 START 4110;
 
 	--Secuencia codigo de informes 
-CREATE SEQUENCE if not EXISTs cod_info_seq INCREMENT 1 START 5110;
+CREATE SEQUENCE IF NOT EXISTs cod_info_seq INCREMENT 1 START 5110;
 
 --Crear tablas
-CREATE TABLE miembros (
+CREATE TABLE IF NOT EXISTs miembros (
 	id_miembro integer UNIQUE DEFAULT nextval('id_miemb_seq'),
   	cc INTEGER UNIQUE,
 	nombre varchar (50) NOT NULL,
@@ -24,21 +24,21 @@ CREATE TABLE miembros (
   	PRIMARY KEY (id_miembro, cc)
 );
 
-CREATE TABLE administrador (
+CREATE TABLE IF NOT EXISTs administrador (
 	id_admin integer NOT NULL,
 	id integer NOT NULL,
 	PRIMARY KEY (id_admin, id),
 	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
-CREATE TABLE vendedores (
+CREATE TABLE IF NOT EXISTs vendedores (
 	id_vendedor integer NOT NULL,
 	id integer NOT NULL,
 	PRIMARY KEY (id_vendedor, id),
 	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
-CREATE TABLE transacciones (
+CREATE TABLE IF NOT EXISTs transacciones (
 	num_unico integer UNIQUE NOT NULL,
 	fecha time NOT NULL,
 	id integer NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE transacciones (
 	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
-CREATE TABLE productos (
+CREATE TABLE IF NOT EXISTs productos (
 	codigo integer PRIMARY KEY DEFAULT nextval('id_prod_seq'),
 	nombre varchar (50) NOT NULL,
 	p_venta_u integer NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE productos (
 	cantidad integer NOT NULL	
 );
 
-CREATE TABLE proovedores (
+CREATE TABLE IF NOT EXISTs proovedores (
 	id_proovedor INTEGER PRIMARY KEY,
   	nombre VARCHAR (50) not NULL,
   	descripcion VARCHAR (100) not NULL,
@@ -63,26 +63,26 @@ CREATE TABLE proovedores (
     direccion VARCHAR (100) NOT NULL
 );
 
-CREATE TABLE administrador (
+CREATE TABLE IF NOT EXISTs administrador (
 	id_admin serial PRIMARY KEY,
 	id_miembro integer NOT NULL,
 	FOREIGN KEY (id_miembro) REFERENCES miembros (id_miembro)
 );
 
-CREATE TABLE vendedores (
+CREATE TABLE IF NOT EXISTs vendedores (
 	id_vendedor integer PRIMARY KEY DEFAULT nextval('id_vend_seq'),
 	id_miembro integer NOT NULL,
 	FOREIGN KEY (id_miembro) REFERENCES miembros (id_miembro)
 );
 
-CREATE TABLE transacciones (
+CREATE TABLE IF NOT EXISTs transacciones (
 	num_unico integer PRIMARY KEY DEFAULT nextval('id_transac_seq'),
 	fecha DATE NOT NULL,
 	id_miembro integer NOT NULL,
 	FOREIGN KEY (id_miembro) REFERENCES miembros (id_miembro)
 );
 
-CREATE TABLE informes (
+CREATE TABLE IF NOT EXISTs informes (
 	codigo INTEGER PRIMARY KEY DEFAULT nextval('cod_info_seq'),
   	ultilidad INTEGER not NULL,
   	total_costos INTEGER not NULL,
@@ -91,7 +91,7 @@ CREATE TABLE informes (
   	FOREIGN KEY (num_unico) REFERENCES transacciones(num_unico)
 );
 
-CREATE TABLE transaccionProducto (
+CREATE TABLE IF NOT EXISTs transaccionProducto (
 	num_unico integer NOT NULL,
 	codigo integer NOT NULL,
 	PRIMARY KEY (num_unico, codigo),
@@ -99,7 +99,7 @@ CREATE TABLE transaccionProducto (
 	FOREIGN KEY (codigo) REFERENCES productos (codigo)
 ); 
 
-CREATE TABLE productoProovedor (
+CREATE TABLE IF NOT EXISTs productoProovedor (
 	id_proovedor INTEGER NOT NULL,
   	codigo INTEGER NOT NULL,
   	PRIMARY KEY (id_proovedor, codigo),
