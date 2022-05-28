92f12717e26114782a65bfe17b8e8e83521e67ff
Rosa0611 <rosero.rosa@correounivalle.edu.co>
2022-05-27 19:47:50 -0500

    Update schemas.sql
    add transacciones

--- schemas.sql
+++ schemas.sql
@@ -2,15 +2,24 @@ CREATE TABLE miembros (
 	id integer NOT NULL PRIMARY KEY,
 	nombre varchar (20) NOT NULL,
 	telefono integer NOT NULL,
-	direccion varchar (20) NOT NULL,
+	direccion varchar (20) NOT NULL
 );
 
 CREATE TABLE administrador (
 	id_admin integer NOT NULL PRIMARY KEY,
-	id_miembro integer NOT NULL,
-	FOREIGN KEY (id_miembro) REFERENCES miembros (id_miembro)
+	id integer NOT NULL,
+	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
 CREATE TABLE vendedores (
+	id_vendedor integer NOT NULL PRIMARY KEY,
+	id integer NOT NULL,
+	FOREIGN KEY (id) REFERENCES miembros (id)
+);
 
+CREATE TABLE transacciones (
+	num_unico integer NOT NULL PRIMARY KEY,
+	fecha time NOT NULL,
+	id integer NOT NULL,
+	FOREIGN KEY (id) REFERENCES miembros (id)
 );
