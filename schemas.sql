3a52b4752098acb29d355fac876daff2547561df
Rosa0611 <rosero.rosa@correounivalle.edu.co>
2022-05-27 20:36:15 -0500

    Update schemas.sql
    AGREGUE ALGO

--- schemas.sql
+++ schemas.sql
@@ -1,34 +1,39 @@
 CREATE TABLE miembros (
 	id integer NOT NULL PRIMARY KEY,
-	nombre varchar (50) NOT NULL,
+	nombre varchar (20) NOT NULL,
 	telefono integer NOT NULL,
-	direccion varchar (50) NOT NULL
+	direccion varchar (20) NOT NULL
 );
 
 CREATE TABLE administrador (
 	id_admin integer NOT NULL,
 	id integer NOT NULL,
-	PRIMARY KEY (id_admin, id)
+	PRIMARY KEY (id_admin, id),
 	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
 CREATE TABLE vendedores (
-	id_vendedor integer NOT NULL PRIMARY KEY,
+	id_vendedor integer NOT NULL,
 	id integer NOT NULL,
+	PRIMARY KEY (id_vendedor, id),
 	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
 CREATE TABLE transacciones (
-	num_unico integer NOT NULL PRIMARY KEY,
+	num_unico integer UNIQUE NOT NULL,
 	fecha time NOT NULL,
 	id integer NOT NULL,
+	PRIMARY KEY (num_unico, id),
 	FOREIGN KEY (id) REFERENCES miembros (id)
 );
 
+drop table transacciones 
+SELECT * FROM transacciones;
+
 CREATE TABLE productos (
 	codigo integer NOT NULL PRIMARY KEY,
-	nombre varchar (50) NOT NULL,
-	p_venta_u integer NOT NUL,
+	nombre varchar (20) NOT NULL,
+	p_venta_u integer NOT NULL,
 	p_compra_u integer NOT NULL,
 	descripcion varchar (100) NOT NULL,
 	cantidad integer NOT NULL	
@@ -40,9 +45,4 @@ CREATE TABLE transaccionProductos (
 	PRIMARY KEY (num_unico, codigo),
 	FOREIGN KEY (num_unico) REFERENCES transacciones (num_unico),
 	FOREIGN KEY (codigo) REFERENCES productos (codigo)
-);
-
---El computador de Mercy no funciona, el mio si :)
-
---grosero
-
+); 
