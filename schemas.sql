528523a43c39227c9aa014f7b5654bfb5d78312c
VicSan9 <105955466+VicSan9@users.noreply.github.com>
2022-05-27 19:09:00 -0500

    Add files via upload

---
+++ schemas.sql
@@ -0,0 +1,12 @@
+CREATE TABLE miembros (
+	id integer NOT NULL PRIMARY KEY,
+	nombre varchar (20) NOT NULL,
+	telefono integer NOT NULL,
+	direccion varchar (20) NOT NULL,
+);
+
+CREATE TABLE administrador (
+	id_admin integer NOT NULL PRIMARY KEY,
+	id_miembro integer NOT NULL,
+	FOREIGN KEY (id_miembro) REFERENCES miembros (id_miembro)
+);
