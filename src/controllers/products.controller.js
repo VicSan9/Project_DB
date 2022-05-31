502e79f40e42bb2bd406e9e0745cd81531426ae7
VicSan9 <svictorvf910@gmail.com>
2022-05-31 14:20:45 -0500

    Update products.controller.js

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -10,11 +10,11 @@ const getProduct = (req, res) => {
 
 const createProduct = async (req, res) => {
 
-    const {nombre, p_venta_u, p_compra_u, lote, descripcion, cantidad, fecha_vencimiento} = req.body;
+    const {nombre, p_venta_u, p_compra_u, lote, descripcion, stack, fecha_vencimiento} = req.body;
     
     try {
-        const result = await pool.query('INSERT INTO productos (nombre, p_venta_u, p_compra_u, lote, descripcion, cantidad, fecha_vencimiento) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', 
-        [nombre, p_venta_u, p_compra_u, lote, descripcion, cantidad, fecha_vencimiento]);
+        const result = await pool.query('INSERT INTO productos (nombre, p_venta_u, p_compra_u, lote, descripcion, stack, fecha_vencimiento) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', 
+        [nombre, p_venta_u, p_compra_u, lote, descripcion, stack, fecha_vencimiento]);
                                      
         res.json(result.rows[0]);
     } catch (error){
