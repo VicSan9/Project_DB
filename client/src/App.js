df3f2dc3a2598eca3961e56d13c3cc20613cf18c
VicSan9 <svictorvf910@gmail.com>
2022-06-03 04:53:45 -0500

    u

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import { Container } from "@mui/material";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Container>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
       </Routes>
+      </Container>
     </BrowserRouter>
   )
 }
