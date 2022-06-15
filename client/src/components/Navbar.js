e260bd34c3c3904ed6083983b798ccea4b49c10d
VicSan9 <svictorvf910@gmail.com>
2022-06-15 02:06:08 -0500

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -146,20 +146,15 @@ export default function Navbar() {
                                 Editar administrador
                             </BootstrapDialogTitle>
                             <DialogContent dividers>
-                                <Typography gutterBottom>
-                                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                                    consectetur ac, vestibulum at eros.
-                                </Typography>
-                                <Typography gutterBottom>
-                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-                                    Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-                                </Typography>
-                                <Typography gutterBottom>
-                                    Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-                                    magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-                                    ullamcorper nulla non metus auctor fringilla.
-                                </Typography>
+                                <Grid>
+                                    <Card boxShadow='none'>
+                                        <CardContent>
+                                            <div>
+                                                :D
+                                            </div>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
                             </DialogContent>
                             <DialogActions>
                                 <Button autoFocus onClick={handleClose}>
